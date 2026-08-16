b295174ae416fe218fcaacedb46bca08439c076d
Yoann Faraus <yofardev@gmail.com>
2026-08-16 14:19:08 +0200

    feat: initialize firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 import 'core/di/service_locator.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await setupServiceLocator();
   runApp(const MyApp());
 }
